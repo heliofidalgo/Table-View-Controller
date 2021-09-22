ff8ef0c0acfc819cc88726b6e7b0054376ea3d00
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2021-09-22 16:07:24 +0100

    Table View Controller
    Estudo da table view controller, criar e desenvolver listas com texto e imagens.

--- Table View Controller/ViewController.swift
+++ Table View Controller/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var dados: [String] = ["Lasanha", "Pizza", "Torta"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dados.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celulaReuso = "celulaReuso"
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath)
+        celula.textLabel?.text = dados[indexPath.row]
+        
+        return celula
+    }
 
 }
 
