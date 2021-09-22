8634a6728cf79c68815357f6bfc8a2750dd142f4
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2021-09-22 15:31:28 +0100

    Initial Commit

---
+++ Table View Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View Controller
+//
+//  Created by Helio Marcus Nery Fidalgo on 22/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
